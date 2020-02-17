b45d887a76f21be3a97ef06e39cda9e45576da36
Daipayan Guha <47353711+DaipayanGuha@users.noreply.github.com>
2020-02-17 23:52:41 +0530

    Add files via upload

---
+++ CGMovieWithSongsSpringBootRESTJPAData/src/main/java/com/cg/moviewithsongs/CgMovieWithSongsSpringBootRestjpaDataApplication.java
@@ -0,0 +1,13 @@
+package com.cg.moviewithsongs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgMovieWithSongsSpringBootRestjpaDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgMovieWithSongsSpringBootRestjpaDataApplication.class, args);
+	}
+
+}
